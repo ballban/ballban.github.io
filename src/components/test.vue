39255ef084407d228b3185821a76540ad5abd971
ballban <ballban@hotmail.com>
2023-02-01 01:27:55 +0900

    test

---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>
+      what the fuck is this
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestTest',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
